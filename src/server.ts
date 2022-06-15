da0e34cac327dcae9f4b37fe6fff6ea7b5db1457
luizphelip3 <luiz_phelip3@hotmail.com>
2022-06-15 10:03:21 -0300

    feat: added create category method into project

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
 app.use(express.json())
 
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log('Server is running at port 3333!'))
