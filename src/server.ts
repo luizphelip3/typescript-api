81d72e580f54e82bc1e5e0663812df55028590e3
luizphelip3 <luiz_phelip3@hotmail.com>
2022-06-15 09:55:11 -0300

    feat: diagram added

--- src/server.ts
+++ src/server.ts
@@ -4,15 +4,5 @@ const app = express();
 
 app.use(express.json())
 
-app.get('/', (req: Request, res: Response) => {
-    return res.send({ message: 'Tá rodando, mlk!' })
-})
-
-app.post("/courses", (req, res) => {
-    const { name } = req.body;
-
-    
-    return res.json({name})
-})
 
 app.listen(3333, () => console.log('Server is running at port 3333!'))
