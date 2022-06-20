b0902381af6ed8653d0c8bd358cc1804de4457d8
luizphelip3 <luiz_phelip3@hotmail.com>
2022-06-20 12:08:00 -0300

    feat: added respository and category dto

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,18 @@
 import { Router } from 'express';
 import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
 
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository
 
 categoriesRoutes.post("/", (req, res) => {
 
     const { name, description } = req.body
 
-    const category: Category = new Category();
+   const result =  categoriesRepository.create({name, description})
 
-    Object.assign(category, {
-        name,
-        description,
-        created_at: new Date()
-    })
-
-    categories.push(category)
-
-    return res.status(201).send(category)
+    return res.status(201).send(result)
 })
 
 export { categoriesRoutes };
