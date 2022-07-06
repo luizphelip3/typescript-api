950145b654b24ee45da03829c58db4ee3dadd032
luizphelip3 <luiz_phelip3@hotmail.com>
2022-07-06 14:20:40 -0300

    feat: added importCategory file usecase method

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,25 @@
-import { Router } from 'express';
-import { createCategoryController } from '../modules/cars/useCases/createCategory';
-import { listCategoriesController } from '../modules/cars/useCases/listCategories';
+import { Router } from "express";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
+
+import multer from "multer";
 
 const categoriesRoutes = Router();
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (req, res) => {
-    return createCategoryController.handle(req, res);
-})
+  return createCategoryController.handle(req, res);
+});
 
-categoriesRoutes.get('/', (req, res) => {
-    return listCategoriesController.handle(req, res);
-})
+categoriesRoutes.get("/", (req, res) => {
+  return listCategoriesController.handle(req, res);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
+  return importCategoryController.handle(req, res);
+});
 
 export { categoriesRoutes };
-
--- src/server.ts
+++ src/server.ts
@@ -7,5 +7,4 @@ app.use(express.json())
 
 app.use(router)
 
-
 app.listen(3333, () => console.log('Server is running at port 3333!'))
